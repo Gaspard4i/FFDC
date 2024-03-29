fee3b5b845cb978c227ed7e4c4ff37e0559477fa
GASPARD CATRY <gaspard.catry.etu@univ-lille.fr>
2024-03-30 00:05:45 +0100

    css & readme & tiles

--- FinnFoundDaChest.md
+++ FinnFoundDaChest.md
@@ -1,4 +1,5 @@
-# Finn Found Da Chest
+# Finn Found Da Chest 
+![](./res/readme/finnrunright.gif)
 
 Finn Found Da Chest is a 2D adventure game inspired by the Adventure Time universe. In this game, players control Finn the Human as he explores a mysterious world filled with obstacles and challenges.
 
@@ -12,45 +13,45 @@ Finn Found Da Chest is a 2D adventure game inspired by the Adventure Time univer
 
 ## How to Play ![FinnStay](./res/readme/finnstay.gif)
 
-### ![Boots](./res/objects/boots.png) Controls: 
+### ![Boots](./res/objects/boots000.png) Controls: 
 - Movement: ZQSD or arrow keys
 - Toggle Debug Mode: T (Press once to show player coordinates and latency, press again to hide)
 - Debugging:
-  - Show FPS: Displayed in the terminal
+  - Show FPS while playing: Displayed in the terminal
   - Show Latency: Displayed in the terminal (when debug mode is toggled)
   - Show Player Coordinates: Displayed in the terminal (when debug mode is toggled)
 
-### ![Chest](./res/objects/chest.png)  Goal: 
+### ![Chest](./res/objects/chest000.png)  Goal: 
 - Explore the world to find keys that unlock doors.
 - Use keys to unlock doors and progress through the game.
 - Find the treasure chest and unlock it with a key to win the game.
 
-### ![Key](./res/objects/key.png) Notes: 
+### ![Key](./res/objects/key000.png) Notes: 
 - The game is a speedrun-style adventure.
 - Graphics feature custom sprites of Finn created by Gaspard Catry.
 - The map layout is fixed and not randomly generated.
 - Additional features and improvements may be implemented in the future, but no updates are planned at this time.
 
-## Installation: ![FinnRun](./res/readme/finnrunright.gif)
+## Installation:
 
 ## Generating Pandoc
 To generate the Pandoc HTML output, use the following command:
 
-  ```bash pandoc FinnFoundDaChest.md -o FinnFoundDaChest.html --css=./css/style.css --standalone
+  ```bash pandoc FinnFoundDaChest.md -o FinnFoundDaChest.html --css=./css/style.css --standalone --section-divs
   ```
   
 ### Prerequisites:objectsur local machine.
-2. Navigate to the project directory.
-3. Compile the Java files:
+1. Navigate to the project directory.
+2. Compile the Java files:
     ```bash
     javac -d ./bin ./src/*.java
     ```
-4. Run the game:
+3. Run the game:
     ```bash
     java -cp ./bin:./res main.Main
     ```
 
-5. Run the game with the run command:
+4. Run the game with the run command:
     ```bash
     bash ./run
     ```
@@ -63,6 +64,6 @@ To generate the Pandoc HTML output, use the following command:
 ## Author & Links
 - Author: Gaspard Catry
 - [Link to GitLab Repository](https://gitlab.univ-lille.fr/gaspard.catry.etu)
-- My instagram : 
-[![Instagram](./res/css-image/qr-code.png)](https://www.instagram.com/gaspard4i/)
+- Linktree
+[![Linktree](./res/css-image/qrcode.png)](https://linktr.ee/Gaspard4i)
 
--- src/tile/TileManager.java
+++ src/tile/TileManager.java
@@ -35,7 +35,7 @@ public class TileManager {
         tile = new Tile[100]; // number of tile we need
         mapTileNum = new int[gp.maxWorldCol][gp.maxWorldCol]; // map
         getTileImage();
-        loadMap("/res/maps/WorldMap4.txt");
+        loadMap("/res/maps/WorldMapTest.txt");
     }
 
     /**
@@ -43,71 +43,91 @@ public class TileManager {
      */
     public void getTileImage(){
 
-        setup(0, "grass001", false); // simple grass
-        setup(1, "grass001", false); // simple grass
-        setup(2, "grass001", false); // simple grass
-        setup(3, "grass001", false); // simple grass
-        setup(4, "grass001", false); // simple grass
-        setup(5, "grass001", false); // simple grass
-        setup(6, "grass001", false); // simple grass
-        setup(7, "grass001", false); // simple grass
-        setup(8, "grass001", false); // simple grass
-        setup(9, "grass001", false); // simple grass     
 
-        setup(10, "grass001", false); // simple grass
-        setup(11, "grass000", false); // grass
+        // GRASS 
+        setup(0, "grass00", false); // old light grass empty
+        setup(1, "grass01", false); // old light grass
+        setup(2, "grass000", false); // new dark grass empty
+        setup(3, "grass001", false); // new dark grass
 
-        setup(12, "water00", true); // water
-        setup(13, "water01", true); // water
-        setup(14, "water02", true); // water
-        setup(15, "water03", true); // water
-        setup(16, "water04", true); // water
-        setup(17, "water05", true); // water
-        setup(18, "water06", true); // water
-        setup(19, "water07", true); // water
-        setup(20, "water08", true); // water
-        setup(21, "water09", true); // water
-        setup(22, "water10", true); // water
-        setup(23, "water11", true); // water
-        setup(24, "water12", true); // water
-        setup(25, "water13", true); // water
-        
-        
-        setup(26, "road000", false); // road
-        setup(27, "road001", false); // road
-        setup(28, "road002", false); // road
-        setup(29, "road003", false); // road
-        setup(30, "road004", false); // road
-        setup(31, "road005", false); // road
-        setup(32, "road006", false); // road
-        setup(33, "road007", false); // road
-        setup(34, "road008", false); // road
-        setup(35, "road009", false); // road
-        setup(36, "road010", false); // road
-        setup(37, "road011", false); // road
-        setup(38, "road012", false); // road
+        // ROAD
+        setup(4, "road000", false); // new simple road
+        setup(5, "road001", false); // new top right corner road 
+        setup(6, "road002", false); // new top road 
+        setup(7, "road003", false); // new top left corner road 
+        setup(8, "road004", false); // new left road 
+        setup(9, "road005", false); // new right road 
+        setup(10, "road006", false); // new bottom left corner road 
+        setup(11, "road007", false); // new bottom road 
+        setup(12, "road008", false); // new bottom corner road 
+        setup(13, "road009", false); // new bottom right little corner road
+        setup(14, "road010", false); // new bottom left little corner road 
+        setup(15, "road011", false); // new up right little corner road 
+        setup(16, "road012", false); // new left right little corner road 
 
-        setup(39, "earth", false); // earth
 
-        setup(40, "wall000", true); // wall
+        // TREE & MUSHROOM
+        setup(17, "tree", true); // old light tree (old grass)
+        setup(18, "tree000", true); // new light blue tree
+        setup(19, "tree001", true); // new dark blue tree
+        setup(20, "tree002", true); // new red tree
+        setup(21, "tree003", true); // new dead tree
+        setup(22, "mushroomtree000", true); // new mushroom tree
+ 
+         // WATER
+        setup(23, "water00", true); // old ocean water empty
+        setup(24, "water01", true); // old ocean water reflexion
+        setup(25, "water02", true); // old ocean water top left corner
+        setup(26, "water03", true); // old ocean water top
+        setup(27, "water04", true); // old ocean water top right corner
+        setup(28, "water05", true); // old ocean water 
+        setup(29, "water06", true); // old ocean water
+        setup(30, "water07", true); // old ocean water
+        setup(31, "water08", true); // old ocean water
+        setup(32, "water09", true); // old ocean water
+        setup(33, "water10", true); // old ocean water
+        setup(34, "water11", true); // old ocean water
+        setup(35, "water12", true); // old ocean water
+        setup(36, "water13", true); // old ocean water
+        setup(37, "water000", true); // new water
+        setup(38, "water001", true); // new water
+        setup(39, "water002", true); // new water
+        setup(40, "water003", true); // new water
+        setup(41, "water004", true); // new water
+        setup(42, "water005", true); // new water
+        setup(43, "water006", true); // new water
+        setup(44, "water007", true); // new water
+        setup(45, "water008", true); // new water
+        setup(46, "water009", true); // new water
+        setup(47, "water010", true); // new water
+        setup(48, "water011", true); // new water
+        setup(49, "water012", true); // new water
+        setup(50, "water013", true); // new water
 
-        setup(41, "tree000", true); // tree
+        // EXTERIOR DECORATIONS 
+        setup(51, "earth", false); // old earth
+        setup(52, "earth000", false); // new earth 
+        setup(53, "wall", true); // old wall 
+        setup(54, "wall000", true); // new wall no wine
+        setup(55, "wall001", true); // new wall blue wine
+        setup(56, "wall002", true); // new wall red wine
+        setup(57, "wall003", true); // new wall dead wine
+        setup(58, "wall004", true); // new wall green wine
 
-        setup(42, "floor01", false); // floor
+        // INTERIOR DECORATIONS
+        setup(59, "floor01", false); // old floor
+        setup(60, "table01", true); // old table
+        setup(61, "hut", true); // old hut (light grass)
+        setup(62, "hut000", true); // new hut (new grass)
 
-        setup(43, "table01", true); // table
 
-        setup(44, "hut000", true); // hut
+        // fake
+        setup(63, "water000", false); // fake water
+        setup(64, "water005", false); // fake water
+        setup(65, "wall001", false); // fake wall
+        setup(66, "tree001", false); // fake tree
 
-        setup(45, "water00", false); // fake water
-
-        setup(46, "water05", false); // fake water
-
-        setup(47, "wall001", false); // fake wall
-
-        setup(48, "tree001", false); // fake tree
     }
-
     /**
      * Sets up a tile with the given index, image path, and collision property.
      * @param index The index of the tile.
@@ -125,6 +145,7 @@ public class TileManager {
                 tile[index].collision = collision;
 
         } catch (IOException e) {
+            System.out.println("/res/tiles/"+imagePath+".png");;
             e.printStackTrace();
         }
     }
